d95a599f3bab109738df9daf41ab9953e5275b57
Koravich Sangkaew <41057176+kwankoravich@users.noreply.github.com>
2022-07-16 16:53:33 +0100

    Update dataset.py

--- trainer/dataset.py
+++ trainer/dataset.py
@@ -149,7 +149,8 @@ class OCRDataset(Dataset):
         self.own_path = 'all_data/en_train_filtered'
 #         self.df = pd.read_csv(os.path.join(root,'train.csv'), sep='^([^,]+),', engine='python', usecols=['id', 'txt'], keep_default_na=False)
 #         self.df = pd.read_csv(os.path.join(root,'labels.csv'), sep='\t', engine='python', usecols=['id', 'txt'], keep_default_na=False)
-        self.df = pd.read_csv(os.path.join(self.own_path,'label.csv'), engine='python', usecols=['id', 'txt'], keep_default_na=False)
+#         self.df = pd.read_csv(os.path.join(self.own_path,'label.csv'), engine='python', usecols=['id', 'txt'], keep_default_na=False)
+        self.df = pd.read_csv(os.path.join(self.own_path,'labels.csv'), engine='python', usecols=['id', 'txt'], keep_default_na=False)
         self.nSamples = len(self.df)
 
         if self.opt.data_filtering_off:
