531e4e1557764504ca355190af036773d33e0321
tao-sg <42664560+tao-sg@users.noreply.github.com>
2022-10-02 23:57:00 +0700

    Update recognition.py

--- easyocr/recognition.py
+++ easyocr/recognition.py
@@ -180,7 +180,7 @@ def get_recognizer(recog_network, network_params, character,\
                 pass
     else:
         model = torch.nn.DataParallel(model).to(device)
-        model.load_state_dict(torch.load("best_accuracy_4.pth", map_location=device))
+        model.load_state_dict(torch.load("best_accuracy.pth", map_location=device))
 
     return model, converter
 
